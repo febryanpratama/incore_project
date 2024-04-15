723f892f226b34dbc83258d502444749af8f2814
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-04-15 13:22:49 +0700

    feat: rev method api tripay

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('transaction/callback', [LandingController::class, 'callback']);
+Route::post('transaction/callback', [LandingController::class, 'callback']);
