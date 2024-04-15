88753bfcfbc6764b664d5e85ff7d8ae0248cb28e
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-04-15 13:21:40 +0700

    feat: rev api

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
-Route::post('api/transaction/callback', [LandingController::class, 'callback']);
+Route::get('transaction/callback', [LandingController::class, 'callback']);
