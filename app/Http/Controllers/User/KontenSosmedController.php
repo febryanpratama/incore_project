36042b7ba5aadc01fbb98573f5084966e6f98af3
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-06-12 04:20:17 +0700

    u

--- app/Http/Controllers/User/KontenSosmedController.php
+++ app/Http/Controllers/User/KontenSosmedController.php
@@ -21,7 +21,7 @@ class KontenSosmedController extends Controller
 
         $responseInstagram = $this->instagramService->getFeed();
 
-        dd($responseInstagram);
+        dd($responseInstagram[0]->caption);
 
         $ListData = [];
 
