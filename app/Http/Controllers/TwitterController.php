0334ac53a78d8c13e17d4fa962dc7f2a78a0860d
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-06-12 05:26:37 +0700

    feat twitterController

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -26,25 +26,39 @@ class TwitterController extends Controller
         // dd($user);
 
         $json = [
+            'user' => [
+                'uid' => $user->id,
+                'nickname' => $user->nickname,
+                'name' => $user->name,
+                'email' => $user->email,
+                'imageUrl' => $user->avatar_original,
+            ],
             'token' => $user->token,
             'tokenSecret' => $user->tokenSecret,
-            'id' => $user->id,
-            'nickname' => $user->nickname,
-            'name' => $user->name,
-            'email' => $user->email,
-            'avatar' => $user->avatar,
-            'avatar_original' => $user->avatar_original,
         ];
+
+        $account = Account::create([
+            'user_id' => Auth::user()->id,
+            'nama_sosmed' => 'twitter '.$user->name,
+            'token' => $user->token,
+            'app' => 'Twitter',
+            'temp_credentials' => null,
+            'status' => 'Active',
+            'token_serialize_tweet' => null,
+            'data' => json_encode($json),
+        ]);
+
+        return redirect('user/account-sosmed')->withSuccess('Twitter Account Connected');
 
         // dd($json);
         
-        dd($json,$user,$user->token, $user->tokenSecret);
+        // dd($json,$user,$user->token, $user->tokenSecret);
         // $user->token
         // $user->tokenSecret
 
         // Handle the authenticated user data
         // For example, create or update the user in the database
 
-        return redirect()->route('home'); // Redirect to the desired route
+        // return redirect()->route('home'); // Redirect to the desired route
     }
 }
