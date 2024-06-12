c96550f1cde73dfb97da1b8ea77ce8f979f134b5
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-06-12 07:41:25 +0700

    feat: rev

--- resources/views/asset/1712027580_contact.blade.php
+++ resources/views/asset/1712027580_contact.blade.php
@@ -120,96 +120,17 @@
 
                             <nav class="primary-menu with-arrows">
                                 <ul class="menu-container">
-                                    <li class="menu-item">
-                                        <a class="menu-link" href="demo-seo.html"><div>Home</div></a>
-                                    </li>
-                                    <li class="menu-item">
-                                        <a class="menu-link" href="demo-seo-about.html"><div>What We Do</div></a>
-                                    </li>
-                                    <li class="menu-item mega-menu">
-                                        <div class="menu-link"><div>Services</div></div>
-                                        <div class="mega-menu-content mega-menu-style-2 px-0">
-                                            <div class="container">
-                                                <div class="row">
-                                                    <a href="demo-seo-optimizations.html" class="mega-menu-column sub-menu-container col-lg-4 border-bottom h-bg-light py-4">
-                                                        <div class="feature-box">
-                                                            <div class="fbox-icon mb-2">
-                                                                <img src="demos/seo/images/icons/seo.svg" alt="Feature Icon" class="bg-transparent rounded-0" />
-                                                            </div>
-                                                            <div class="fbox-content">
-                                                                <h3 class="text-transform-none ls-0">Seo Optimization</h3>
-                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, ipsa!</p>
-                                                            </div>
-                                                        </div>
-                                                    </a>
-                                                    <a href="demo-seo-social.html" class="mega-menu-column sub-menu-container col-lg-4 border-bottom h-bg-light py-4">
-                                                        <div class="feature-box">
-                                                            <div class="fbox-icon mb-2">
-                                                                <img src="demos/seo/images/icons/social.svg" alt="Feature Icon" class="bg-transparent rounded-0" />
-                                                            </div>
-                                                            <div class="fbox-content">
-                                                                <h3 class="text-transform-none ls-0">Social Media Marketing</h3>
-                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, ipsa!</p>
-                                                            </div>
-                                                        </div>
-                                                    </a>
-                                                    <a href="demo-seo-adwords.html" class="mega-menu-column sub-menu-container col-lg-4 border-bottom h-bg-light py-4">
-                                                        <div class="feature-box">
-                                                            <div class="fbox-icon mb-2">
-                                                                <img src="demos/seo/images/icons/adword.svg" alt="Feature Icon" class="bg-transparent rounded-0" />
-                                                            </div>
-                                                            <div class="fbox-content">
-                                                                <h3 class="text-transform-none ls-0">Adwords Campaign</h3>
-                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, ipsa!</p>
-                                                            </div>
-                                                        </div>
-                                                    </a>
-                                                    <a href="demo-seo-experience.html" class="mega-menu-column sub-menu-container col-lg-4 border-bottom h-bg-light py-4">
-                                                        <div class="feature-box">
-                                                            <div class="fbox-icon mb-2">
-                                                                <img src="demos/seo/images/icons/experience.svg" alt="Feature Icon" class="bg-transparent rounded-0" />
-                                                            </div>
-                                                            <div class="fbox-content">
-                                                                <h3 class="text-transform-none ls-0">Customer Experience</h3>
-                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, ipsa!</p>
-                                                            </div>
-                                                        </div>
-                                                    </a>
-                                                    <a href="demo-seo-analysis.html" class="mega-menu-column sub-menu-container col-lg-4 border-bottom h-bg-light py-4">
-                                                        <div class="feature-box">
-                                                            <div class="fbox-icon mb-2">
-                                                                <img src="demos/seo/images/icons/analysis.svg" alt="Feature Icon" class="bg-transparent rounded-0" />
-                                                            </div>
-                                                            <div class="fbox-content">
-                                                                <h3 class="text-transform-none ls-0">Digital Analysis</h3>
-                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, ipsa!</p>
-                                                            </div>
-                                                        </div>
-                                                    </a>
-                                                    <a href="demo-seo-content-marketing.html" class="mega-menu-column sub-menu-container col-lg-4 border-bottom h-bg-light py-4">
-                                                        <div class="feature-box">
-                                                            <div class="fbox-icon mb-2">
-                                                                <img src="demos/seo/images/icons/content_marketing.svg" alt="Feature Icon" class="bg-transparent rounded-0" />
-                                                            </div>
-                                                            <div class="fbox-content">
-                                                                <h3 class="text-transform-none ls-0">Content Marketing</h3>
-                                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, ipsa!</p>
-                                                            </div>
-                                                        </div>
-                                                    </a>
-                                                    <div class="mega-menu-column sub-menu-container col-12 p-0">
-                                                        <a href="#" class="button button-rounded button-large text-transform-none ls-0 w-100 text-center m-0 rounded-0 py-3 button-light">View All <i class="bi-arrow-right"></i></a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="menu-item">
-                                        <a class="menu-link" href="demo-seo-faqs.html"><div>FAQs</div></a>
-                                    </li>
-                                    <li class="menu-item current">
-                                        <a class="menu-link" href="demo-seo-contact.html"><div>Contact</div></a>
-                                    </li>
+                                    @if (App\Helpers\Format::getNavigation($store_slug) != NULL)
+                                        @foreach (App\Helpers\Format::getNavigation($store_slug) as $item)
+                                        <li class="menu-item current">
+                                            <a class="menu-link" href="{{ url('/'.$store_slug."/".$item->slug) }}">
+                                                <div>{{ $item->nama_navigation }}</div>
+                                            </a>
+                                        </li>
+                                        @endforeach
+                                    @else
+                                        
+                                    @endif
                                 </ul>
                             </nav>
                             <form class="top-search-form" action="https://canvastemplate.com/search.html" method="get">
